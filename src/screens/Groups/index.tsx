4650d8e499564acbb3ffdd0986d4bba7a91f62ae
diegouidev <diegocoelho44@gmail.com>
2023-01-26 14:17:09 -0300

    criando a primeira rota da aplicação

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { FlatList } from 'react-native';
+import { useNavigation } from '@react-navigation/native'
 
 import { Header } from '@components/Header';
 import { Container } from './styles';
@@ -8,8 +9,14 @@ import { GroupCard } from '@components/GroupCard';
 import { ListEmpty } from '@components/ListEmpty';
 import { Button } from '@components/Button';
 
+
 export function Groups() {
-const [groups, setGroups] = useState<string[]>([])
+  const [groups, setGroups] = useState<string[]>([])
+  
+  const navigation = useNavigation()
+  function handleNewGroup() {
+    navigation.navigate('new')
+  }
 
   return (
     <Container>
@@ -35,6 +42,7 @@ const [groups, setGroups] = useState<string[]>([])
       />
       <Button
         title='Criar nova turma'
+        onPress={handleNewGroup}
       />
     </Container>
   );
--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -1,3 +1,5 @@
+import { useNavigation } from '@react-navigation/native'
+
 import { Input } from "@components/Input";
 import { Button } from "@components/Button";
 import { Header } from "@components/Header";
@@ -5,12 +7,18 @@ import { Highlight } from "@components/Highlight";
 import { Container, Content, Icon } from "./styles";
 
 export function NewGroup() {
+
+  const navigation = useNavigation()
+  function handleGroup() {
+    navigation.navigate('group')
+  }
+
   return (
     <Container>
       <Header showBackButton />
 
       <Content>
-        <Icon />
+        <Icon onPress={handleGroup} />
         <Highlight
           title='Nova turma'
           subtitle="Crie a turma para adicionar as pessoas"
